cf5f442d63bdfabe5f6f04e1b64114187184c6c7
Kartik Shankhdhar <kartikshankhdhar2007@gmail.com>
2026-07-08 16:31:34 +0000

    done

--- 01_basics/06_nums_math.js
+++ 01_basics/06_nums_math.js
@@ -1,1 +1,16 @@
-console.log("hello world")
+console.log("today we learn about the Numbers and Maths")
+
+const score =458
+console.log(score)
+
+//another way 
+const balance= new Number(100)
+console.log(balance)
+
+console.log("Number Function")
+
+// Number Functions
+console.log(balance.toFixed(3))
+console.log(balance.toString())
+console.log(balance.valueOf())
+console.log(balance.toExponential(15))
