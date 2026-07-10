6342789e5e35f361064ce5d6ec1b6e562f60dfce
Kartik Shankhdhar <kartikshankhdhar2007@gmail.com>
2026-07-10 18:33:26 +0000

    done

--- 01_basics/06_nums_math.js
+++ 01_basics/06_nums_math.js
@@ -13,4 +13,31 @@ console.log("Number Function")
 console.log(balance.toFixed(3))
 console.log(balance.toString())
 console.log(balance.valueOf())
-console.log(balance.toExponential(15))
+console.log(balance.toExponential(3))
+
+const num= 10000000
+console.log(num.toLocaleString('en-IN'))
+
+const anum =123.456
+console.log(anum.toPrecision(5))
+
+//++++++++++++++++++++++++ Maths +++++++++++++++++++++++++++++++++++++++++++++++++
+console.log("Now we studing abhout maths")
+
+console.log(Math.abs(-421));
+console.log(Math.round(22.485894));
+console.log(Math.floor(22.485894));
+console.log(Math.ceil(22.485894));
+
+console.log(Math.sqrt(1225))
+console.log(Math.min(4,2,4,9,6,7,5,))
+console.log(Math.max(4,2,4,9,6,7,5,))
+
+/////============================= "Random Function " ====================================
+//random function give the value between 0 and 1
+
+console.log(Math.random());
+console.log("lets make dice")
+const max=6
+const min=1
+console.log(Math.floor(Math.random()*(max-min+1)) + min)
---
+++ 01_basics/07_dates.js
@@ -0,0 +1,13 @@
+/// Dates
+
+let myDate= new Date();
+console.log(myDate);
+console.log(myDate.toDateString());
+console.log(myDate.toISOString());
+console.log(myDate.toJSON());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toLocaleString());
+console.log(myDate.toLocaleTimeString());
+console.log(myDate.toTimeString());
+console.log(myDate.toUTCString());
+console.log(myDate.toString());
